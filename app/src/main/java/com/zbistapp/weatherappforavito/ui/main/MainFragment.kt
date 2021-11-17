bc5b536efcc96661eb1d34cd63333b1dc9769b40
zbist <zbist3@gmail.com>
2021-11-17 15:51:38 +0300

    Create toolbar with option menu for latest location search

--- app/src/main/java/com/zbistapp/weatherappforavito/ui/main/MainFragment.kt
+++ app/src/main/java/com/zbistapp/weatherappforavito/ui/main/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.zbistapp.weatherappforavito.ui.main
 
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.zbistapp.weatherappforavito.R
 
@@ -7,5 +9,33 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     companion object {
         fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search -> {
+                //todo
+                return true
+            }
+            R.id.my_location -> {
+                //todo
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
